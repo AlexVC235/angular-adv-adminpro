06ff50d6b3d6a0fa9ca8a5eda4db54aabf9d7b49
AlexVC235 <alejandro.vera.c@uni.pe>
2023-11-10 16:38:38 -0500

    Fin s4

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    Grafica1Component,
+    PagesComponent,
+    ProgressComponent,
+  ],
+  exports: [
+    DashboardComponent,
+    Grafica1Component,
+    PagesComponent,
+    ProgressComponent,
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule,
+  ]
+})
+export class PagesModule { }
